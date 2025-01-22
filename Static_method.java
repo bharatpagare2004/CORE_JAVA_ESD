c5dcf8b51dc9c4369dff226ed6a0adc2f9cb8e20
bharatpagare2004 <bharatpagare018@gmail.com>
2025-01-22 21:43:34 +0530

    static

---
+++ Static_method.java
@@ -0,0 +1,15 @@
+public class Static_method {
+
+    public static int addtwono(int a , int b)
+    {
+        return a+b;
+        
+    }
+    public static void main(String[] args) {
+        int a =addtwono(23, 90);
+        System.out.println(a); // withoud class name
+        
+
+    }
+    
+}
